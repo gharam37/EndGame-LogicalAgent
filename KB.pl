806ced62432ad2556fe3d6f8768b1d8822ffc151
gzakaria <romy.zakaria@gmail.com>
2019-11-09 15:32:30 +0200

    Generating grid

---
+++ KB.pl
@@ -0,0 +1,7 @@
+limits(5,5).
+ironman(1,2,0,s).
+thanos(3,4,s).
+stone(1,1,s).
+stone(2,1,s).
+stone(2,2,s).
+stone(3,3,s).
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,54 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 public class Main {
 
 	public static void GenGrid (String grid)
 	{
 		try {
-		      File myObj = new File("filename.pl");
+			
+		      File myObj = new File("KB.pl");
+
 		      
 		      if (myObj.createNewFile()) {
 		        System.out.println("File created: " + myObj.getName());
+			      String[] parts = grid.split(";");
+			      
+					
+				String Dimension="limits("+Integer.parseInt(parts[0].split(",")[0])+","+
+						Integer.parseInt(parts[0].split(",")[1])+").";
+				
+				System.out.println(Dimension);
+				String IronMan="ironman("+Integer.parseInt(parts[1].split(",")[0])+","+
+						Integer.parseInt(parts[1].split(",")[1])+",0,s).";
+				String thanos="thanos("+Integer.parseInt(parts[2].split(",")[0])+","+
+						Integer.parseInt(parts[2].split(",")[1])+",s).";
+				
+				
+				BufferedWriter writer = new BufferedWriter(new FileWriter("KB.pl"));
+				writer.append(Dimension);
+				writer.append("\n");
+				writer.append(IronMan);
+				writer.append("\n");
+				writer.append(thanos);
+				
+				
+				String[] StonesStrings = parts[3].split(",");
+		    	for(int i=0;i+1<StonesStrings.length;i+=2)
+		    	{
+		    		String stone="stone("+Integer.parseInt(StonesStrings[i])+","+
+		    				Integer.parseInt(StonesStrings[i+1])+",s).";
+		    		writer.append("\n");
+					writer.append(stone);
+		    		
+		    	}
+
+				
+				writer.close();
+
+						
+
 		      } else {
 		        System.out.println("File already exists.");
 		      }
@@ -24,6 +64,6 @@ public class Main {
 	
 	public static void main(String[]args) {
 		System.out.println("Hello");
-		GenGrid("Hi");
+		GenGrid("5,5;1,2;3,4;1,1,2,1,2,2,3,3");
 	}
 }
